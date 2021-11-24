36e1bad270f05a39d8a4016239e2ae275c31e3d3
David Sozashvili <davidSozashvili@gmail.com>
2021-11-24 21:06:05 +0400

    feat: adding Componenets - (Meals, AvailableMeals, MealsSummary)

--- src/componenets/Layout/HeaderCartButton.js
+++ src/componenets/Layout/HeaderCartButton.js
@@ -1,13 +1,11 @@
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartButton.module.css';
-
-
+import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton() {
   return (
     <button className={classes.button}>
       <span className={classes.icon}>
-          <CartIcon />
+        <CartIcon />
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>3</span>
